04db1cf8ca39198c69c82f2892f532c2f6c831bd
Janno Esko <eskojanno@gmail.com>
2025-01-07 20:37:10 +0200

    Add the player ID into the getstatus message

--- code/server/sv_main.c
+++ code/server/sv_main.c
@@ -586,8 +586,8 @@ static void SVC_Status( netadr_t from ) {
         cl = &svs.clients[i];
         if ( cl->state >= CS_CONNECTED ) {
             ps = SV_GameClientNum( i );
-            Com_sprintf (player, sizeof(player), "%i %i \"%s\"\n",
-                ps->persistant[PERS_SCORE], cl->ping, cl->name);
+            Com_sprintf (player, sizeof(player), "%i %i %i \"%s\"\n",
+                i, ps->persistant[PERS_SCORE], cl->ping, cl->name);
             playerLength = strlen(player);
             if (statusLength + playerLength >= sizeof(status) ) {
                 break;      // can't hold any more
